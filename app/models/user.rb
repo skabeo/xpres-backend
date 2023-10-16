6fb4035ed6da5a946baedc90d51ed35f73b24a0c
skabeo <spencerokyere95@gmail.com>
2023-10-16 21:28:10 +0000

    Add active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_one_attached :image
 end
