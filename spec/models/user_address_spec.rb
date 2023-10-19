1ba7214cc2cb02902ba419be8215595659dfee44
skabeo <spencerokyere95@gmail.com>
2023-10-19 10:26:52 +0000

    Fix linters error

--- spec/models/user_address_spec.rb
+++ spec/models/user_address_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UserAddress, type: :model do
   describe 'User address model' do
-
-    user = User.create(:first_name => 'Eug', :last_name => 'basa', :email => 'jane@doe.com', :password => '123456')
+    user = User.create(first_name: 'Eug', last_name: 'basa', email: 'jane@doe.com', password: '123456')
     user_address = UserAddress.create(
-      user_id: user.id, 
-      address: 'B10140/12 Obonu street', 
-      city: 'Acra', 
-      tel_line: '0507690136', 
+      user_id: user.id,
+      address: 'B10140/12 Obonu street',
+      city: 'Acra',
+      tel_line: '0507690136',
       tel_line2: '0244104094'
     )
 
@@ -28,12 +27,12 @@ RSpec.describe UserAddress, type: :model do
     end
 
     it 'tel_line2 can be blank' do
-      user = User.create(:first_name => 'Eug', :last_name => 'basa', :email => 'jane2@doe.com', :password => '123456')
+      user = User.create(first_name: 'Eug', last_name: 'basa', email: 'jane2@doe.com', password: '123456')
       user_address = UserAddress.create(
-        user_id: user.id, 
-        address: 'B10140/12 Obonu street', 
-        city: 'Acra', 
-        tel_line: '0507690136', 
+        user_id: user.id,
+        address: 'B10140/12 Obonu street',
+        city: 'Acra',
+        tel_line: '0507690136',
         tel_line2: nil
       )
       expect(user_address).to be_valid
