eec4367be4a49b41a879bca7d9c2240d4a7fbb02
skabeo <spencerokyere95@gmail.com>
2023-10-20 08:02:52 +0000

    Add order tests

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  describe 'Order model' do
+    user = User.first
+    product = Product.last
+    payment = Payment.first
+
+    quantity = rand(15..25)
+    total_order = product.price.to_i * quantity
+
+    subject { Order.new(
+      user_id: user.id, 
+      product_id: product.id, 
+      payment_id: payment.id, 
+      quantity: quantity, 
+      total: total_order
+    ) }
+
+    before { subject.save }
+
+    it 'quantity should be present' do
+      subject.quantity = nil 
+      expect(subject).to_not be_valid
+    end
+
+    it 'quantity should be an integer' do
+      subject.quantity = 'hey'
+      expect(subject).to_not be_valid
+    end
+
+    it 'total should be present' do
+      subject.total = nil 
+      expect(subject).to_not be_valid
+    end
+
+    it 'total should be an integer' do
+      subject.total = 'hey'
+      expect(subject).to_not be_valid
+    end
+  end
+end
