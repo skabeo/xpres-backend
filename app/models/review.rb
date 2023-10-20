7e1a573423681cca0b2044a4101b9face42198e9
skabeo <spencerokyere95@gmail.com>
2023-10-20 08:37:15 +0000

    Add review model test

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-  validates :rating, presence: true, numericality: true
+  validates :rating, presence: true, numericality: true, inclusion: { in: [1, 2, 3, 4, 5] }
   validates :comment, presence: true, length: { maximum: 100 }
 end
