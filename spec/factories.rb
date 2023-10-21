5c470e8e0a656a008eed142985386b28c9d28871
skabeo <spencerokyere95@gmail.com>
2023-10-21 14:00:16 +0000

    Fix  linters errors

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :payment do
-    provider { Faker::Subscription.payment_method } 
-    status { "Approved" } 
-    ref { Faker::Number.number(digits: 10) } 
+    provider { Faker::Subscription.payment_method }
+    status { 'Approved' }
+    ref { Faker::Number.number(digits: 10) }
     amount { Faker::Number.decimal(l_digits: 3) }
   end
 end
