53bb80a9c41c760f40b285cca461adfa753a320c
skabeo <spencerokyere95@gmail.com>
2023-10-24 15:09:24 +0000

    refactor get payment request test

--- spec/factories.rb
+++ spec/factories.rb
@@ -16,3 +16,12 @@ FactoryBot.define do
     shipping_cost { rand(100..500) }
   end
 end
+
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { '123456' }
+  end
+end
--- spec/integrations/payments/get_payment_spec.rb
+++ spec/integrations/payments/get_payment_spec.rb
@@ -2,18 +2,47 @@ require 'rails_helper'
 
 RSpec.describe 'Payment', type: :request do
   describe 'GET /index' do
-    before do
-      FactoryBot.create_list(:payment, 9)
-      get '/api/v1/payments'
+    context 'can view all payment when user is an admin' do
+
+      before do
+        @admin_user = FactoryBot.create(:user, role: 'admin')
+        sign_in @admin_user
+
+        FactoryBot.create_list(:payment, 9)
+        get '/api/v1/payments'
+      end
+
+      it 'returns all payments' do
+        response_data = JSON.parse(response.body)
+        expect(response_data.size).to eq(10)
+      end
+
+      it 'should return status 200' do
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it 'returns all payments' do
-      response_data = JSON.parse(response.body)
-      expect(response_data.size).to eq(10)
+    context 'access restricted as a normal user' do
+      before do
+        @user = FactoryBot.create(:user)
+        sign_in @user
+
+        get '/api/v1/payments'
+      end
+
+      it 'should have forbidden http status' do
+        expect(response).to have_http_status(:forbidden)
+      end
     end
 
-    it 'should return status 200' do
-      expect(response).to have_http_status(:success)
+    context 'user should be unathorized when not signed in' do
+      before do
+        get '/api/v1/payments'
+      end
+
+      it 'should have http status of unathorized' do
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 end
