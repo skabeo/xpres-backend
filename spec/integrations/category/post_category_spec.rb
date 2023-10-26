ae24d107761e5e02e5979425b60cc2e1794d5af6
skabeo <spencerokyere95@gmail.com>
2023-10-26 08:40:31 +0000

    Add user sessions spec

--- spec/integrations/category/post_category_spec.rb
+++ spec/integrations/category/post_category_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'Category', type: :request do
   describe 'POST /categories' do
     let(:response_body) { JSON.parse(response.body) }
     context 'with valid parameters' do
-
       before do
         post '/api/v1/categories', params: {
           category: {
